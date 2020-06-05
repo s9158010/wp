eda98c7a1a540b7ed14488f8ce575260188baadf
ccckmit <ccckmit@gmail.com>
2020-06-05 11:03:12 +0800

    add grepAllRef.js

--- code/08-app2/basic/split1.js
+++ code/08-app2/basic/split1.js
@@ -5,7 +5,3 @@ let str = 'name: ccc, Tel: 082-333333, Email:ccckmit@gmail.com, url:https://misa
 // let regexp = /[A-Za-z]+:/g
 let regexp = /\w+:/g
 var m
-while ((m = regexp.exec(str))) {
-  console.log(`Found ${m[0]}. Next starts at ${regexp.lastIndex}.`);
-}
-
